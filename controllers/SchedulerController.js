2c3f1fe29b2ddc9483c5866aee61f10ec6e7925d
Nur Rohim <nur.rohim@gmedia.co.id>
2021-10-12 13:34:19 +0700

    update url heroku

--- controllers/SchedulerController.js
+++ controllers/SchedulerController.js
@@ -1,12 +1,15 @@
 const amqp = require('amqplib') 
 var axios = require('axios');
+const INTERVAL_DURATION = 60000;
+const url = 'amqps://bdqryjuc:FctmJfyXZXG1syIAx8EKZaRzEBmVv5h-@clam.rmq.cloudamqp.com/bdqryjuc' || "amqp://localhost";
 
 module.exports = {
 
   wa:async(req, res)=>{
     try{
       const { senderid, number, pesan } = req.body
-      amqp.connect('amqp://localhost')
+      amqp.connect(url)
+      // amqp.connect('amqp://localhost')
       .then(conn => {
         return conn.createChannel().then(ch => {
           const data = {
@@ -18,7 +21,7 @@ module.exports = {
           res.status(200).json({message:"Successfully send message"});
         })
         .finally(() => {
-          setTimeout(function() { conn.close(); }, 500);
+          setTimeout(function() { conn.close(); }, INTERVAL_DURATION);
         })
       })
     } catch(err){
