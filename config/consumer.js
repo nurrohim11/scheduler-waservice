d3d33e2d07a0ef87b53e4ab494e8cea036f25cf3
Nur Rohim <nur.rohim@gmedia.co.id>
2021-10-13 08:10:35 +0700

    update noack

--- config/consumer.js
+++ config/consumer.js
@@ -9,42 +9,40 @@ amqp.connect(url)
     return conn.createChannel().then(ch => {
 
       const queue1 = ch.assertQueue('schedulerwa', {durable:false})
-      if(queue1){
-        queue1.then(()=>{
-          return ch.consume('schedulerwa', async(msg)=>{
-            let dataqueue = JSON.parse(msg.content.toString())
-            console.log('dataqueue queue ',dataqueue)
+      queue1.then(()=>{
+        return ch.consume('schedulerwa', async(msg)=>{
+          let dataqueue = JSON.parse(msg.content.toString())
+          console.log('dataqueue queue ',dataqueue)
 
-            var data = JSON.stringify({
-              "sender": dataqueue.sender,
-              "message": dataqueue.message,
-              "number": dataqueue.number
-            });
+          var data = JSON.stringify({
+            "sender": dataqueue.sender,
+            "message": dataqueue.message,
+            "number": dataqueue.number
+          });
 
-            var config = {
-              method: 'post',
-              url: 'http://116.254.117.132:801/send-message',
-              headers: { 
-                'Client-Service': 'perkasa-sales', 
-                'Auth-Key': 'gmedia', 
-                'Nik-Ga': '930034', 
-                'Timestamp': '', 
-                'Signature': '', 
-                'User-Id': '', 
-                'Content-Type': 'application/json'
-              },
-              data : data
-            };
+          var config = {
+            method: 'post',
+            url: 'http://116.254.117.132:801/send-message',
+            headers: { 
+              'Client-Service': 'perkasa-sales', 
+              'Auth-Key': 'gmedia', 
+              'Nik-Ga': '930034', 
+              'Timestamp': '', 
+              'Signature': '', 
+              'User-Id': '', 
+              'Content-Type': 'application/json'
+            },
+            data : data
+          };
 
-            axios(config)
-            .then(function (response) {
-              console.log(JSON.stringify(response.data));
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
+          axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data));
           })
+          .catch(function (error) {
+            console.log(error);
+          });
         })
-      }
+      }, {noAck:true})
     })
 }).catch(console.warn)
